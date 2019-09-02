b8c50d1635df66dd321d88b136fd95d9a8ea089d
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-09-02 23:35:32 +0900

    Create migration
    create_categories_table、create_posts_table、create_comments_tableのマイグレーションファイルの作成。

---
+++ database/migrations/2019_09_02_142049_create_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            //nullable指定をすると、指定した型だけでなくNULLも渡せるようになる。
+            $table->string('category_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
