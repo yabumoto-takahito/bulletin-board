12894154253b34255cf9aa9a0ae0a8e92dfd5c2a
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-09-05 00:13:36 +0900

    Create a FormRequest
    フォームリクエスト(バリデーション)の作成。

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+
 use App\Post;
 
 class PostController extends Controller
@@ -36,9 +38,9 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
-        //
+        return redirect('/');
     }
 
     /**
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,13 +9,28 @@
         </div>
     @endif
 
+    <!-- バリデーションエラー表示 -->
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <div class="card">
         <div class="card-body">
-            <form>
+            <form action=" {{ route('posts.store') }} " method="post">
+                @csrf
+
+                <!-- タイトル入力欄 -->
               <div class="form-group">
                 <label for="exampleInputTitle1">title</label>
-                <input type="text" class="form-control" id="exampleInputTitle1" placeholder="タイトル">
+                <input type="text" class="form-control" id="exampleInputTitle1" placeholder="タイトル" name="title">
               </div>
+              <!-- カテゴリー入力欄 -->
               <div class="form-group">
                 <label for="exampleInput1">category</label>
                 <select class="form-control" id="exampleInputCategory" placeholder="カテゴリー" name="category_id">
@@ -25,10 +40,14 @@
                     <option value="3">love</option>
                 </select>
               </div>
+              <!-- 本文 -->
               <div class="form-group">
                 <label for="exampleInputTitle1">content</label>
-                <textarea type="text" rows="5" class="form-control" id="exampleInputContent" placeholder="本文"></textarea>
+                <textarea type="text" rows="5" class="form-control" id="exampleInputContent" placeholder="本文" name="content"></textarea>
               </div>
+              <!-- ユーザーID追加 -->
+              <input type="hidden" name="user_id" value="{{ Auth::id() }}">
+              <!-- 送信 -->
               <button type="submit" class="btn btn-primary">送信する</button>
             </form>
         </div>
