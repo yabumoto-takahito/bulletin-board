8fe2694546e2ca651986940144889d39497efa8e
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-09-09 23:57:49 +0900

    Create tags and post_tag tables
    tagsテーブルとpost_tagテーブルの作成。

---
+++ database/migrations/2019_09_09_125929_create_post_tag_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            // ->index()：カラムにインデックスを追加
+            // ->unsigned()：整数カラムを符号なしに設定(MySQLのみ)
+            $table->bigInteger('post_id')->unsigned()->index();
+            $table->bigInteger('tag_id')->unsigned()->index();
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+}
