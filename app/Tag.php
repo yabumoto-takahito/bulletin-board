51c73d440932ca47c1eaa938b6ca4c9270fd1ef7
yabumoto-takahito <yammo1107@yahoo.co.jp>
2019-09-10 00:07:54 +0900

    Set relation of Tag Model
    Tagモデルのリレーションを設定。

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+        // 所謂ホワイトリスト。$fillableに指定したカラムのみ、create()やfill()、update()で値が代入される。
+        protected $fillable = [
+          'tag_name'
+    ];
 }
